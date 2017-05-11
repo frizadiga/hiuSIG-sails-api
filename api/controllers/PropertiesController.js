7b626b0a5122d5a9552a501a9127d118c416fae7
frizadiga <frizadiga@gmail.com>
2017-05-11 17:46:18 +0700

    fix typo

--- api/controllers/PropertiesController.js
+++ api/controllers/PropertiesController.js
@@ -1,5 +1,5 @@
 /**
- * PropertyController
+ * PropertiesController
  *
  * @description :: Server-side logic for managing properties
  * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
